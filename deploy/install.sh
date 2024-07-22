63a9ad5a1b23d27fffbd941e397549bf1626a9de
gabrielcrispimvs <gabriel.crispimvs@gmail.com>
2024-07-22 12:22:10 -0300

    scripts de deploy

---
+++ deploy/install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+cd ~
+if ! [ -d sistemas_distribuidos ]
+then
+    mkdir sistemas_distribuidos
+fi
+cd sistemas_distribuidos
+
+if ! [ -d trabalho-sistemas-distribuidos ]
+then
+    git clone https://github.com/gabrielcrispimvs/trabalho-sistemas-distribuidos.git
+    cd trabalho-sistemas-distribuidos
+    git checkout rpc
+else
+    cd trabalho-sistemas-distribuidos
+    git checkout rpc
+    git pull
+fi
+
+pip install rpyc
+
+echo Killing previous process
+pkill -f datanode.py
+
+echo Starting datanode.py
+python3 datanode.py localhost 18811 &
+
+disown
